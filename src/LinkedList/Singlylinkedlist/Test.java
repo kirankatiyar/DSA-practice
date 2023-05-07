754f6befb51bd234cccd5ffd22bf28ac72e765ea
kirankatiyar <kirankatiyar16@gmail.com>
2023-05-07 14:15:06 +0530

    Added example of insertion At given position(singly linkedlist)

---
+++ src/LinkedList/Singlylinkedlist/Test.java
@@ -0,0 +1,72 @@
+package LinkedList.Singlylinkedlist;
+public class Test {
+    Node head;
+    int size=0;
+    //Node class
+    class Node{
+        String data;
+        Node next;
+        //Parameterized Constructor
+        Node(String data){
+            this.data=data;
+            this.next=null;
+            size++;
+        }
+    }
+    public void insertNode(String data){
+        Node new_node = new Node(data);
+        if(head==null){
+            head=new_node;
+            return;
+        }
+        Node tem = head;
+        while(tem.next!=null){
+            tem=tem.next;
+        }
+        tem.next=new_node;
+    }
+    public void insertAtPosition(int pos,String data){
+        Node new_node = new Node(data);
+        if(pos<1|| pos>size+1){
+            System.out.println("invalid position , insertion not possible at this position");
+        }
+
+            else if(pos==1){
+                new_node.next=head;
+                head=new_node;
+            }
+        else {
+            Node temp = head;
+            Node newnode = new Node(data);
+            for(int i=1;i<=pos-1;i++){
+                temp=temp.next;
+            }
+            newnode.next=temp.next;
+            temp.next=newnode;
+        }
+    }
+    public void displayList(){
+        if(head==null){
+            System.out.println("list is empty");
+        }
+        Node curr = head;
+        while(curr!=null){
+            System.out.print(curr.data+" ");
+            curr=curr.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Test obj = new Test();
+        obj.insertNode("node_n1");
+        obj.insertNode("node_n2");
+        obj.insertNode("node_n3");
+        obj.insertNode("node_n4");
+        System.out.println("show original list");
+        obj.displayList();
+        obj.insertAtPosition(3,"node_p");
+        System.out.println("show list after insertion at given position");
+        obj.displayList();
+    }
+}
