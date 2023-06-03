bb5decbda2f8a6ff10b095ca3682599d7816863e
kirankatiyar <kirankatiyar16@gmail.com>
2023-06-04 05:09:50 +0530

    Added program of queue,enqueue element in queue using linked list

---
+++ src/Queue/EnqueueElementInQueueUsingLinkedList.java
@@ -0,0 +1,46 @@
+package Queue;
+public class EnqueueElementInQueueUsingLinkedList {
+    Node front = null;
+    Node rear = null;
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public void insertElement(int data){
+        Node new_node = new Node(data);
+        if(front==null&&rear==null){
+            front=rear=new_node;
+        }
+        else{
+            rear.next=new_node;
+            rear=new_node;
+            rear.next=null;
+        }
+    }
+    public void displayQueue(){
+        if(front==null&&rear==null){
+            System.out.println("queue is empty");
+        }
+        Node temp=front;
+        while(temp!=null){
+            System.out.print(temp.data+"  ");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        EnqueueElementInQueueUsingLinkedList refobj = new EnqueueElementInQueueUsingLinkedList();
+        System.out.println("enter element in queue and show queue");
+        refobj.insertElement(10);
+        refobj.insertElement(20);
+        refobj.insertElement(30);
+        refobj.insertElement(40);
+        refobj.insertElement(50);
+        refobj.displayQueue();
+
+    }
+}
